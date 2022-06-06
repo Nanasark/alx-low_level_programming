d68cd97a1a2ebf64ae2a79fc2d760ca289190763
Daniel S.N Ayensu <47149163+Nanasark@users.noreply.github.com>
2022-06-06 21:47:50 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+
+/**
+ *
+ *  * Comment here.
+ *
+ *   * Betty shit style.
+ *
+ *    */
+
 int main(void)
+
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
-
-
