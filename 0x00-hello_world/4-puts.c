ae30c270c1225ec07a128b095e283ea68e14fb92
Daniel S.N Ayensu <47149163+Nanasark@users.noreply.github.com>
2022-06-06 22:19:42 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
+ * Description: main - Prints out a string to stdout
+ * comments in C source code.
+ * Please use it consistently.
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * Description:  A column of asterisks on the left side,
+ * with beginning and ending almost-blank lines.
+ */
+
 int main(void)
 
 {
