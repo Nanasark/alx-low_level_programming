86d76f35e290dd174ca9d142dc92e6d5dc8b11c2
Nanasark <danielnyamekyesarkodie@gmail.com>
2022-06-13 14:46:18 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,30 +7,14 @@
 /**
 
  * main - prints a string
-
- *
-
  * Return: 0
-
  */
 
-int main(void)
-  
-{
-  
-  char my_string[] = {'_', 'p', 'u', 't', 'c', 'h',
-		      
-		      'a', 'r'};
-  
-
-  
+int main(void){
+  char my_string[] = {'_', 'p', 'u', 't', 'c', 'h','a', 'r'};  
   int x = 0, err_no;
-  
-
-  
-  while (x <= 7)
-    
-    {
+ 
+  while (x <= 7){
       
       err_no = _putchar(my_string[x]);
       
