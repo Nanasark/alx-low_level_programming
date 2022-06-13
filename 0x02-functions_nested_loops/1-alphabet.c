4d5010449f2946d6c66ce0cc8f74106d86cd1d1e
Nanasark <danielnyamekyesarkodie@gmail.com>
2022-06-13 15:34:27 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+#include <ctype.h>
+
+
+/**
+ * print_alphabet - prints alphabets
+ */
+
+void print_alphabet(void)
+
+{
+
+	char my_alpha[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+
+		'X', 'Y', 'Z'};
+
+
+	int x = 0;
+
+
+	while (x < 26)
+
+	{
+
+		_putchar(tolower(my_alpha[x]));
+
+		x++;
+
+	}
+
+	_putchar('\n');
+
+}
