c966bf7f62cce9a70dea9d8d81dc45fd4e9077c2
Nanasark <danielnyamekyesarkodie@gmail.com>
2022-06-14 11:06:47 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,27 +1,74 @@
+#include <stdio.h>
+
 #include "main.h"
-void print_2D(int p) { _putchar('0' + (p / 10)); _putchar('0' + (p % 10));
-}
-void print_3D(int p) { int x = p % 100; _putchar('0' + (p / 100)); _putchar('0' + (x / 10)); _putchar('0' + (x % 10));
-}
-void print_char(void) { _putchar(','); _putchar(' ');
-}
-void print_end(void) { _putchar('9'); _putchar('8'); _putchar('\n');
-}
-void print_to_98(int n) { while (n != 98) { if (n >= 0 && n < 10) { _putchar('0' + n); n++;
-		} else if (n < 0 && n > -10)
-		{ _putchar('-'); _putchar('0' + (n * -1)); n++;
-		} else if (n > 9 && n <= 97)
-		{ print_2D(n); n++;
-		} else if (n < -9 && n > -100)
-		{ _putchar('-'); print_2D((n * -1)); n++;
-		} else if (n > 99)
-		{ print_3D(n); n--;
-		} else if (n < -99)
-		{ _putchar('-'); print_3D((n * -1)); n++;
-		} else if (n == 99)
-		{ print_2D(n); n--;
+
+/**
+ * print_to_98 - a function that prints all natural numbers from n to 98
+ * user input's number prints to 98, regardless < 98 or > 98
+ * @n: number input
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+
+{
+
+	if (n < 98)
+
+	{
+
+		while (n <= 98)
+
+		{
+
+			printf("%d", n);
+
+			if (n != 98)
+
+			{
+
+				printf(", ");
+
+			}
+
+			n++;
+
 		}
-		print_char();
+
 	}
-	print_end();
+
+	else if (n > 98)
+
+	{
+
+		while (n >= 98)
+
+		{
+
+			printf("%d", n);
+
+			if (n != 98)
+
+			{
+
+				printf(", ");
+
+			}
+
+			n--;
+
+		}
+
+	}
+
+	else
+
+	{
+
+		printf("98");
+
+	}
+
+	printf("\n");
+
 }
